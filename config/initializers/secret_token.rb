235e8860dcc92e59d6d13b24b2664efab22cac61
bhim subedi <lovelotusist@hotmail.com>
2013-10-29 00:16:23 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nmhidta::Application.config.secret_token = 'f23586a1835bda4753b06362fa97d28ca27bdd192055668e610d048074d05303e4e1c5ac934bce9050073e0648d904920f9fa94c5f3fa1dcf9d859b6a3c547ea'
